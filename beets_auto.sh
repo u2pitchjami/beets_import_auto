bb947b136efa2f568a059ebfa3d4dbba4980f1d7
u2pitchjami <u2pitchjami@gmail.com>
2024-12-15 15:48:25 +0100

    ajouts logs

--- beets_auto.sh
+++ beets_auto.sh
@@ -12,13 +12,26 @@
 #	BUT: auto import beets avec restart                                       #
 #									                                         #
 ############################################################################## 
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+source ${SCRIPT_DIR}/.config.cfg
 
+if [ ! -d $DOSLOG ]
+	then
+	mkdir $DOSLOG
+fi
+
+if [ -f $CONFIG_NORMAL ]; then
+    mv "${CONFIG}" "${CONFIG_MANUEL}"
+    mv "${CONFIG_NORMAL}" "${CONFIG}"
+fi
+echo -e "[`date`] - Démarrage du script import auto beets :" | tee -a $LOG
 source $BEETS_ENV
 if pgrep -x "beet" > /dev/null
 then
-    echo "Running"
+    echo "beets Running" | tee -a $LOG
 else
-    echo "Stopped"
+    echo "beets Stopped" | tee -a $LOG
+    echo "restart beets" | tee -a $LOG
     source $BEETS_ENV
     beet import "${BASE}"
 fi
